a897d3ba596f5bde35206b620504c589d4dbac76
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2017-09-06 13:47:05 -0500

    Added script to start cloudark
    Also added grep filtering in stop-cloudark.sh

---
+++ start-cloudark.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python server/fmserver.py 1>>cld-server.log 2>&1 &
--- stop-cloudark.sh
+++ stop-cloudark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ps -eaf | grep 'python server/fmserver.py' | awk '{print $2}' | xargs kill
+ps -eaf | grep 'python server/fmserver.py' | grep -v grep | awk '{print $2}' | xargs kill
 
