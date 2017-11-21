920a08937a799fc2d78dba12c554044c70587201
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2017-11-20 19:36:04 -0600

    Updating run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -6,3 +6,16 @@ nosetests --tests tests.server.dbmodule.objects.test_app
 echo "Running tests from tests.server.dbmodule.objects.test_env"
 nosetests --tests tests.server.dbmodule.objects.test_env
 
+echo "Running functional tests"
+
+has_server_started=`ps -eaf | grep -v grep | grep fmserver`
+
+if [[ -z "${has_server_started}" ]]; then
+    echo "CloudARK server not running. Please start the server and then run the tests."
+    exit 
+fi
+
+nosetests --tests tests.server.local.test_local:TestLocal.test_app_deploy_hello_world
+
+
+
